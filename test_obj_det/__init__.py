fa8793e19476f61533af7ab30698cd566febd076
codefinity-arsenii-dr <arsenii.drobotenko@codefinity.com>
2025-04-14 20:47:42 +0300

    write init scripts for imports

---
+++ test_obj_det/__init__.py
@@ -0,0 +1,11 @@
+from .utils import draw_bbox, load_data, evaluate_custom_model
+
+# If you have more utilities (e.g., for metric evaluation, loading data), import them here
+# Example:
+# from .utils import some_utility_function
+
+__all__ = [
+    'draw_bbox',
+    'load_data',
+    'evaluate_custom_model'
+]
---
+++ test_obj_det/test/__init__.py
@@ -0,0 +1,17 @@
+from .task1 import hint1, solution1, check1
+from .task2 import hint2, solution2, check2
+from .task3_4_5 import hint3, solution3, hint4, solution4, hint5, solution5, check345
+from .task6 import hint6, solution6, check6
+from .task7 import hint7, solution7, check7
+from .task8 import hint8, solution8, check8
+
+__all__ = [
+    'hint1', 'solution1', 'check1',
+    'hint2', 'solution2', 'check2',
+    'hint3', 'solution3',
+    'hint4', 'solution4',
+    'hint5', 'solution5', 'check345',
+    'hint6', 'solution6', 'check6',
+    'hint7', 'solution7', 'check7',
+    'hint8', 'solution8', 'check8',
+]
